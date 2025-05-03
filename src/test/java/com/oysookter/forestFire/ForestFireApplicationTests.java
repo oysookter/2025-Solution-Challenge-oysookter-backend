b944591c1cf17aa77cc6462438345df8cf586a7c
ha9eun <ha9eunsoo@gmail.com>
2025-05-03 15:29:23 +0900

    Initial Setting

---
+++ src/test/java/com/oysookter/forestFire/ForestFireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oysookter.forestFire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
