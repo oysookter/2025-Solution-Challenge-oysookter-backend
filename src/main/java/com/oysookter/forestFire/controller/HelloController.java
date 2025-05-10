cc9e33a3eee72818c4eff4fa616d25f1b134aba8
ha9eun <ha9eunsoo@gmail.com>
2025-05-10 15:21:18 +0900

    Test: Hello API 로그 추가

--- src/main/java/com/oysookter/forestFire/controller/HelloController.java
+++ src/main/java/com/oysookter/forestFire/controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.oysookter.forestFire.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("테스트 hello API 호출됨");
         return "Hello from Spring Boot!";
     }
 
