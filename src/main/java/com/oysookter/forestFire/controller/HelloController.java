c5cdb9f0f5ef76ee73b8961a7ee3b7b84b1f5407
ha9eun <ha9eunsoo@gmail.com>
2025-05-10 15:26:20 +0900

    Test: sysout으로 로그 변경

--- src/main/java/com/oysookter/forestFire/controller/HelloController.java
+++ src/main/java/com/oysookter/forestFire/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        log.info("테스트 hello API 호출됨");
+        System.out.println("테스트 hello API 호출됨");
         return "Hello from Spring Boot!";
     }
 
